a1b781f14278efbaa22df5f660f9fcc394c9a34a
BChangs99 <yy3chang@edu.uwaterloo.ca>
2018-12-06 23:41:38 -0500

    Add weather API functionality

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props)
 
@@ -9,6 +12,7 @@ export default class SearchBar extends Component {
     }
 
     this.onInputChange = this.onInputChange.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
   }
 
   onInputChange(event) {
@@ -20,7 +24,9 @@ export default class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault()
 
-    //Fetch weather data here
+    //Fetch weather data here and reset term state
+    this.props.fetchWeather(this.state.term)
+    this.setState({term: ''})
   }
 
   render() {
@@ -41,3 +47,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)

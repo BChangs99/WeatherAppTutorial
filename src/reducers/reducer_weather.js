3591ed1825ff00a127ef0ba105bc7c67ec4723a4
BChangs99 <yy3chang@edu.uwaterloo.ca>
2018-12-07 20:36:54 -0500

    Implement reducer functionality for displaying weather API data

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,11 @@
+import FETCH_WEATHER from '../actions/index'
+
+export default function(state = [], action) {
+    switch(action.type) {
+        case FETCH_WEATHER:
+        //This return is flattened, it is not an array with arrays inside, its one array
+            return [ action.payload.data, ...state ]
+    }
+    
+    return state
+}
